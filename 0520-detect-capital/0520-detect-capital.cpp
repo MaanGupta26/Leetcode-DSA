b4421643dd907ebc07e068a5d6cf0fff9084c3ef
MaanGupta26 <maangupta80@gmail.com>
2026-09-11 10:04:34 +0530

    Time: 0 ms (100%), Space: 8.2 MB (50.99%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count =0;
+        for (char c : word) {
+            if (isupper(c)) {
+                 count++;
+            }
+        }
+        if (count == word.length()) {
+            return true;
+
+        }
+        if (count ==0) {
+            return true;
+        }
+        if (count ==1 && isupper(word[0])) {
+            return true;
+        }
+        return false;
+    }
+};
